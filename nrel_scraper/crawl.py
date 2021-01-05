27014d65edcee022f80ed3959753380a54e38ca9
Kim Sha <KSha@Kims-MacBook-Pro.local>
2021-01-04 23:30:18 -0500

    remove venv and requirements.txt

--- nrel_scraper/crawl.py
+++ nrel_scraper/crawl.py
@@ -1,5 +1,5 @@
 import sys, os
-# import imp
+import imp
 import logging
 # from urllib.parse import urlparse
 
@@ -9,8 +9,8 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
 # Need to "mock" sqlite for the process to not crash in AWS Lambda / Amazon Linux
-# sys.modules["sqlite"] = imp.new_module("sqlite")
-# sys.modules["sqlite3.dbapi2"] = imp.new_module("sqlite.dbapi2")
+sys.modules["sqlite"] = imp.new_module("sqlite")
+sys.modules["sqlite3.dbapi2"] = imp.new_module("sqlite.dbapi2")
 
 # def is_in_aws():
 #     return os.getenv('AWS_EXECUTION_ENV') is not None
